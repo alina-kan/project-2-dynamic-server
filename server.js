b86ea3579243159365d2bf3601bc8ced11d8f6ea
alina-kan <alinak10971@gmail.com>
2022-11-05 15:02:05 -0500

    Addition of style.css and renamed html pages

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let template_dir = path.join(__dirname, 'templates');
 let db_filename = path.join(__dirname, 'db', 'powerplant.sqlite3'); // <-- change this
 
 let app = express();
-let port = 8000;
+let port = 5000;
 
 // Open SQLite3 database (in read-only mode)
 let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
@@ -46,6 +46,45 @@ app.get('/year/:selected_year', (req, res) => {
     });
 });
 */
+
+app.get('/location.html/:cid', (req, res) => {
+    console.log(req.params.cid);
+    fs.readFile(path.join(template_dir, 'location.html'), (err, template) => {
+        // modify `template` and send response
+        // this will require a query to the SQL database
+        let query = 'SELECT Country.country_code AS cid, Country.country_name, Company.company_name, Location.longitude, \
+         Location.latitude FROM Country INNER JOIN Company INNER JOIN Location WHERE Country.country_code = ?';
+        let cid = req.params.cid.toUpperCase();
+        db.all(query, [cid], (err, rows) => {
+            //console.log(err);
+            //console.log(rows);
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.write('Error, file not found');
+                res.end();
+            }
+            else {
+                let response = template.toString();
+                response = response.replace('%%COMPANY%%', rows[2].cid);
+                //response = response.replace('%%MFR_IMAGE%%', '/images/' + mfr + '_logo.png');
+                //response = response.replace('%%MFR_ALT_TEXT%%', 'Logo of ' + rows[0].mfr);
+
+                let location_table = '';
+                let i;
+                for(i=0; i < rows.length; i++){
+                    location_table = location_table + '<tr><td>' + rows[i].name + '</td>';
+                    //cereal_table = cereal_table + '<td>' + rows[i].calories + '</td>';
+                    //cereal_table = cereal_table + '<td>' + rows[i].carbohydrates + '</td>';
+                    //cereal_table = cereal_table + '<td>' + rows[i].protein + '</td>';
+                    //cereal_table = cereal_table + '</tr>';
+                }
+                response = response.replace('%%PLANT_INFO%%', location_table);
+                res.status(200).type('html').send(response);
+            }
+        })
+
+    });
+});
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
